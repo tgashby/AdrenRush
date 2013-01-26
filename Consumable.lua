0582962fb508548237920e64a024fc37f104750f
Travis <tnscholz@gmail.com>
2013-01-25 22:16:22 -0800

    Consumables
    added consumables

---
+++ Consumable.lua
@@ -0,0 +1,20 @@
+local Class = require('hump.class')
+local Vector2D = require('hump.vector')
+
+Consumable = Class{inherits = Tile,
+    function(self, img, pos, heartAccel)
+      Tile.construct(self, img, pos)
+      
+      self.heartAccel = heartAccel
+      
+    end
+  }
+   
+    function Consumable:useConsumable()
+      
+      player.heartRate = player.heartRate + self.heartAccel
+      
+    end
+   
+   
+   
