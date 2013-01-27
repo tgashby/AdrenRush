c615c969da8e2c997d1594cc0908d4bcbb38817d
Simon Vurens <svurens@gmail.com>
2013-01-26 17:12:31 -0800

    Consumables

--- Consumable.lua
+++ Consumable.lua
@@ -4,18 +4,24 @@ local Vector2D = require('hump.vector')
 Consumable = Class{inherits = Tile,
     function(self, x, y, amount)
     	local img = Globals.IMAGE_DIR
-
+		self.used = false
     	if amount < 100 then
     		img = img .. "upper_" .. amount .. ".png"
+			self.heartAccel = amount
     	else
     		img = img .. "downers_" .. amount - 100 .. ".png"
+			self.heartAccel = 100 - amount
     	end
 
       	Tile.construct(self, img, Vector2D(x, y), "consumable")
-      	self.heartAccel = amount
+      	
 	end
 }
    
 function Consumable:UseConsumable()
- 	player.heartRate = player.heartRate + self.heartAccel
+	if not self.used then
+		player.heartRate = player.heartRate + self.heartAccel
+		self.used = true
+		self.image = love.graphics.newImage(Globals.IMAGE_DIR .. "floor_0.png")
+	end
 end
--- Part.lua
+++ Part.lua
@@ -3,12 +3,17 @@ local Vector2D = require('hump.vector')
 
 Part = Class{inherits = Tile,
     function(self, x, y, id)
-      local img = Globals.IMAGE_DIR .. "part_" .. id .. ".png"
-
-      Tile.construct(self, img, Vector2D(x, y), "part")
+		local img = Globals.IMAGE_DIR .. "part_" .. id .. ".png"
+		self.id = id
+		self.used = false
+		Tile.construct(self, img, Vector2D(x, y), "part")
 	end
 }
 
 function Part:usePart()
-
+	if not self.used then
+		player.inventory[self.id] = true
+		self.used = true
+		self.image = love.graphics.newImage(Globals.IMAGE_DIR .. "floor_0.png")
+	end
 end
